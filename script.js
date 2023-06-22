478c9b58e6a22df5fc40d0a9ad2a656ccb2b31cd
Typicek <carotkacz333@seznam.cz>
2023-06-22 19:09:17 +0200

    Before branch push

--- script.js
+++ script.js
@@ -4,18 +4,51 @@ const ctx = cnvas.getContext("2d")
 canvas.width = canvas.offsetWidth
 canvas.height = canvas.offsetHeight
 
+window.addEventListener
+
 function canvs_init(){
     ctx.fillStyle = 'white'
     ctx.strokeStyle = 'white'
 
-    ctx.fillRect(0, (canvas.height/2) - 50, 30, 100)
-    ctx.fillRect(canvas.width - 30, (canvas.height/2) - 50, 30, 100)
+    let paddle1 = new Paddle(0, (canvas.height/2) - 50)
+    let paddle2 = new Paddle(canvas.width - 30, (canvas.height/2) - 50)
 
-    ctx.beginPath()
-    ctx.arc((canvas.width / 2), (canvas.height/2), 20, 0, 2 * Math.PI)
-    ctx.fill()
+    paddle1.draw()
+    paddle2.draw()
+
+    let ball = new Ball((canvas.width / 2), (canvas.height/2))
+    ball.draw()
 }
 
+class Ball{
+    constructor(x, y){
+        this.x = x
+        this.y = y
+        this.speedX = 3
+        this.speedY = 3
+    }
+    draw(){
+        ctx.beginPath()
+        ctx.arc(this.x, this.y, 20, 0, 2 * Math.PI)
+        ctx.fill()
+    }
+}
+
+class Paddle{
+    constructor(x, y){
+        this.x = x
+        this.y = y
+        this.speedY = 3
+    }
+    draw(){
+        ctx.fillRect(this.x, this.y, 30, 100)
+        ctx.fillRect(this.x, this.y, 30, 100)
+    }
+    move(direction){
+        if(direction == 1) this.y -= this.speedY
+        if(direction == 2) this.y += this.speedY
+    }
+}
 
 
 // USER CODE
